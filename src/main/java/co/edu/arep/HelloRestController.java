474a979ca145292d3cead2cf4456f6724ac0504f
Santiago Silva <santiago10y66@outlook.es>
2025-09-14 14:03:20 -0500

    Desarrollo Taller

---
+++ src/main/java/co/edu/arep/HelloRestController.java
@@ -0,0 +1,21 @@
+package co.edu.arep;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloRestController {
+
+    private static final String template = "Hello, %s!";
+
+    @GetMapping("/greeting")
+    public String greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return String.format(template, name);
+    }
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello Docker!";
+    }
+}
